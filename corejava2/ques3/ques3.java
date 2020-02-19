e28dfd99e318eeea21305c3f42b7e2a6ffc323d5
Lakshya <60686958+Lakshya0718@users.noreply.github.com>
2020-02-19 23:59:22 +0530

    core java

---
+++ corejava2/ques3/ques3.java
@@ -0,0 +1,15 @@
+package ques3;
+
+//3. WAP to produce NoClassDefFoundError and ClassNotFoundException exception.
+
+public class ques3 {
+    public static void main(String[] args) {
+
+        try{
+
+        }
+        catch (NoClassDefFoundError e){
+            System.out.println(e);
+        }
+    }
+}
