fbd122d0f2301beb6c0c1d5112786c1457e88b65
Lakshya <60686958+Lakshya0718@users.noreply.github.com>
2020-02-24 13:33:19 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -6,12 +6,9 @@ public class q7 {
     static{
         System.out.println("Static block called");
         System.out.println("Lakshya");
-        System.out.println("Sharma");
-        System.out.println(21);
-    }
+        }
 
     static String ftitle = "Static variable";
-    static String fname = "lakshya";
     static  String lname = "sharma";
     static int age = 22 ;
 
@@ -19,16 +16,13 @@ public class q7 {
     public static void main(String[] args) {
 
         System.out.println(q7.ftitle);
-        System.out.println(q7.fname);
-        System.out.println(q7.lname);
         System.out.println(q7.age);
         myfun();
     }
 
     static void myfun(){
-        System.out.println("static function");
-        System.out.println(fname);
+        System.out.println("Static Method");
         System.out.println(lname);
-        System.out.println(age);
+
     }
 }
