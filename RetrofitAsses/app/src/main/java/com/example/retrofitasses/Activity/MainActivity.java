61fe90cadf60136d6f5eb81f3083c1030cb9d8da
lakshya <lakshya.ttn.com>
2020-03-20 20:24:07 +0530

    webservices ass

---
+++ RetrofitAsses/app/src/main/java/com/example/retrofitasses/Activity/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.retrofitasses.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.Glide;
+import com.example.retrofitasses.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button clicktoLoadImage, buttonHttp, reset_button,switch_button;
+    private ProgressBar progressBar;
+    private HttpURLConnection httpURLConnection = null;
+    private URL url;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.image);
+        clicktoLoadImage = findViewById(R.id.text);
+        buttonHttp = findViewById(R.id.button);
+        progressBar = findViewById(R.id.progressbar);
+        reset_button = findViewById(R.id.reset_button);
+        switch_button = findViewById(R.id.switchbtn);
+
+
+        clicktoLoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                Glide.with(MainActivity.this).asBitmap().load("https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg").into(imageView);
+                progressBar.setVisibility(View.GONE);
+
+
+            }
+        });
+
+        buttonHttp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ImageDownloadTask().execute("https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg");
+
+            }
+        });
+
+        reset_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageDrawable(null);
+            }
+        });
+
+        switch_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    public class ImageDownloadTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Bitmap bitmap = null;
+            try {
+                url = new URL(params[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.connect();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+
+
+            }
+
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            imageView.setImageBitmap(result);
+        }
+
+
+    }
+}
